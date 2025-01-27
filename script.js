27fcfdd2fdb63eea3a6380bb4561ddfaaef2a7fc
KojoBolt <kojobolt@gmail.com>
2025-01-27 10:01:14 +0000

    javascript validation form

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("registration-form");
     const feedbackDiv = document.getElementById("form-feedback");
 
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
 
         // Validate user input with trim  
         const username = document.getElementById('username').value.trim();
